ebfdda15a65be25dcb0d9de40b8f08c0dc03ef12
Ilya Chupin <ilya.chupin@itwis.org>
2022-05-25 13:10:13 +0700

    complete lesson 13

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from news.models import News, Category
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'category', 'is_published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+    list_filter = ('is_published', 'category')
+
+
+admin.site.register(News, NewsAdmin)
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(Category, CategoriesAdmin)
