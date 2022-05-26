cc2425a18a36b4ad637080588dc1c9b161fba416
Ilya Chupin <ilya.chupin@itwis.org>
2022-05-26 18:59:38 +0700

    complete lesson 32

--- news/views.py
+++ news/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from news.models import News, Category
 
 
 def index(request):
-    return HttpResponse('Hello world!')
+    news = News.objects.get(title='News 2')
+    category = Category.objects.get(pk=1).news_set.all()
+    return HttpResponse(category)
 
 
 def test(request):
     return HttpResponse('<h1>Test page</h1>')
+
+
+def din_var(request, value):
+    return HttpResponse(f'<h1>Dynamically value is: {value}</h1>')
